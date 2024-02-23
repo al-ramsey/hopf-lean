19e00cbcae471b91268c945f3f69d91e0d49a3cf
al-ramsey <s2158261@ed.ac.uk>
2024-02-23 18:11:30 +0000

    change notation, atrribute simp

--- HopfLean/Bialgebra.lean
+++ HopfLean/Bialgebra.lean
@@ -26,30 +26,17 @@ that the counit and comultiplication are algebra morphisms. -/
 class Bialgebra (R : Type u) (A : Type v) [CommSemiring R] [Semiring A] extends Algebra R A, Coalgebra R A where
   /-- The counit is an algebra morphism -/
   counit_mul : ∀ a₁ a₂ : A, counit (a₁ * a₂) = counit a₁ * counit a₂
-  counit_unit : counit 1 = 1
+  counit_one : counit 1 = 1
   /-- The comultiplication is an algebra morphism -/
   comul_mul : ∀ a₁ a₂ : A, comul (a₁ * a₂) = Algebra.TensorProduct.mul (comul a₁) (comul a₂)
-  comul_unit : comul 1 = 1
+  comul_one : comul 1 = 1
 
 namespace Bialgebra
 variable {R : Type u} {A : Type v}
 variable [CommSemiring R] [Semiring A] [B : Bialgebra R A]
 
-@[simp]
-theorem counit_mul_apply (a₁ a₂ : A) : B.counit (a₁ * a₂) = B.counit a₁ * B.counit a₂ :=
-  counit_mul a₁ a₂
-
-@[simp]
-theorem counit_unit_apply : B.counit 1 = 1 :=
-  counit_unit
-
-@[simp]
-theorem comul_mul_apply (a₁ a₂ : A) : B.comul (a₁ * a₂) = Algebra.TensorProduct.mul (B.comul a₁) (B.comul a₂) :=
-  comul_mul a₁ a₂
-
-@[simp]
-theorem comul_unit_apply : B.comul 1 = 1 :=
-  comul_unit
+-- teach the simplifier these axioms
+attribute [simp] counit_one counit_mul comul_one comul_mul
 
 end Bialgebra
 
@@ -64,8 +51,8 @@ namespace CommSemiring
 noncomputable
 instance toBialgebra : Bialgebra R R where
   counit_mul := by simp
-  counit_unit := rfl
+  counit_one := rfl
   comul_mul := by simp
-  comul_unit := rfl
+  comul_one := rfl
 
 end CommSemiring
